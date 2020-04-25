8438bedc61da15022c9f6346f4912896a378b89d
Vitor_Vendrami <vitor_vendrami@hotmail.com>
2020-04-24 23:05:43 -0300

    inserindo modelo de dados

--- nosso_site/site_teste/models.py
+++ nosso_site/site_teste/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Dado(models.Model):
 	nome = models.CharField(max_length=100)
-	email = models.CharField(max_lenght=100)
+	email = models.CharField(max_length=100)
 	senha = models.CharField(max_length=15)
 
 	def __str__(self):
