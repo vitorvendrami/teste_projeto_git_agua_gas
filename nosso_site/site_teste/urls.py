a0bb920e93a262589201ce6bb443c9892def0aef
Vitor_Vendrami <vitor_vendrami@hotmail.com>
2020-04-25 21:53:18 -0300

    FUNCIONANDO PAGINA LOGIN E NAVEGACAO

--- nosso_site/site_teste/urls.py
+++ nosso_site/site_teste/urls.py
@@ -3,9 +3,9 @@ from .views import *
 
 urlpatterns = [
     path('', login),
-    path('nosso_site/login', login),
+    path('nosso_site/login/', login),
     path('site_teste/confirmar_login/', confirmar_login),
-    path('nosso_site/cadastro', cadastro),
+    path('nosso_site/cadastro/', cadastro),
     path('site_teste/confirmar_cadastro/', confirmar_cadastro)
 
 
